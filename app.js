54e1fea37b41161cb223af8e12fbb362277a782d
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-10-21 11:56:53 +0200

    Refactor grid creation and add reset button

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-const container = document.getElementById("container");
-const size = 16;
+let container = document.getElementById("container");
+let size = 16;
+const reset = document.getElementById("reset");
+reset.addEventListener("click", (e) => {
+  container.innerHTML = "";
+  do {
+    size = prompt("Enter the size of the grid less than or equal to 100");
+  } while (size > 100);
+  makeGrid(size);
+});
 
 for (let i = 0; i < size; i++) {
-  const divItems = document.createElement("div");
+  let divItems = document.createElement("div");
   divItems.classList.add("grid-items");
   container.appendChild(divItems);
   for (let j = 0; j < size; j++) {
-    const div = document.createElement("div");
+    let div = document.createElement("div");
     div.classList.add("grid-item");
     divItems.appendChild(div);
     div.addEventListener("mouseover", (e) => {
       div.classList.toggle("grid-item-hover");
     });
+    // div.addEventListener("mouseleave", (e) => {
+    //   div.classList.remove("grid-item-hover");
+    // });
+  }
+}
+
+function makeGrid(size) {
+  for (let i = 0; i < size; i++) {
+    let divItems = document.createElement("div");
+    divItems.classList.add("grid-items");
+    container.appendChild(divItems);
+    for (let j = 0; j < size; j++) {
+      let div = document.createElement("div");
+      div.classList.add("grid-item");
+      divItems.appendChild(div);
+      div.addEventListener("mouseover", (e) => {
+        div.classList.toggle("grid-item-hover");
+      });
+      // div.addEventListener("mouseleave", (e) => {
+      //   div.classList.remove("grid-item-hover");
+      // });
+    }
   }
 }
