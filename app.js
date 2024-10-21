a76a08da70fe6b9f97d3ac2e351f187e136c0e4c
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-10-21 10:47:18 +0200

    Add initial HTML, CSS, and JavaScript code for Etch-a-Sketch app

---
+++ app.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("container");
+const size = 16;
+
+for (let i = 0; i < size; i++) {
+  const divItems = document.createElement("div");
+  divItems.classList.add("grid-items");
+  container.appendChild(divItems);
+  for (let j = 0; j < size; j++) {
+    const div = document.createElement("div");
+    div.classList.add("grid-item");
+    divItems.appendChild(div);
+  }
+}
